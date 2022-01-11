7d472e9404fcfbe509036e2cc947c402fe453786
dshiple2 <90435108+dshiple2@users.noreply.github.com>
2022-01-10 22:07:33 -0700

    the rest I hope

---
+++ wwwroot/js/mission2.js
@@ -0,0 +1,34 @@
+﻿$("#btnSend").click(function () {
+    var assScore = parseFloat($("#assScore").val());
+    var gpScore = parseFloat($("#gpScore").val());
+    var quizScore = parseFloat($("#quizScore").val());
+    var examScore = parseFloat($("#examScore").val());
+    var intexScore = parseFloat($("#intexScore").val());
+    var finalScore = (assScore * .55) + (gpScore * .05) + (quizScore * .1) + (examScore * .2) + (intexScore * .1);
+    if (finalScore >= 94.0) {
+        var letterGrade = "A";
+    } else if (finalScore >= 90.0) {
+        var letterGrade = "A-";
+    } else if (finalScore >= 87.0) {
+        var letterGrade = "B+";
+    } else if (finalScore >= 84.0) {
+        var letterGrade = "B";
+    } else if (finalScore >= 80.0) {
+        var letterGrade = "B-";
+    } else if (finalScore >= 77.0) {
+        var letterGrade = "C+";
+    } else if (finalScore >= 74.0) {
+        var letterGrade = "C";
+    } else if (finalScore >= 70.0) {
+        var letterGrade = "C-";
+    } else if (finalScore >= 67.0) {
+        var letterGrade = "D+";
+    } else if (finalScore >= 64.0) {
+        var letterGrade = "D";
+    } else if (finalScore >= 60.0) {
+        var letterGrade = "D-";
+    } else {
+        var letterGrade = "E";
+    }
+    alert("Numeric Score: " + finalScore + "\nLetter Grade: " + letterGrade);
+})
