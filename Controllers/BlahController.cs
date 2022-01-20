f2a70b9d5f39555818b0a67633008c5d87963d97
dshiple2 <90435108+dshiple2@users.noreply.github.com>
2022-01-19 19:43:56 -0700

    pls

---
+++ Controllers/BlahController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MIssion2.Models;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace MIssion2.Controllers
+{
+    public class BlahController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult calculator()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult calculator (CalculatorModel model)
+        {
+            return View();
+        }
+    }
+}
